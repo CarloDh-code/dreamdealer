77ee05d6350cb06af664bf6a17b602702822f37b
Carlo <carlodhlln@gmail.com>
2024-11-18 16:22:12 +0100

    add dream controller

---
+++ app/controllers/dream_controller.rb
@@ -0,0 +1,43 @@
+class DreamController < ApplicationController
+before_action :set_dream, only: [:show]
+
+  def index
+    @dreams = Dream.all
+  end
+
+  def show
+  end
+
+  def new
+    @dream = Dream.new
+  end
+
+  def create
+    @dream = Dream.new(dream_params)
+    if dream.save
+      redirect_to dream_path(@dream)
+    else
+      render :new, :unprocessable_entity
+    end
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_dream
+    @dream = Dream.find(params[:id])
+  end
+
+  def dream_params
+    params.require(:dream).permit(:title, :description, :price_per_night, :category, :age_limit, :number_of_roles)
+  end
+
+end
