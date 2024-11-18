cb39b5b2647d1c48118bebcb78b0cc5125a0d1de
mickael bitane <bitane.mickael@gmail.com>
2024-11-18 15:34:51 +0100

    add dream model

---
+++ app/models/dream.rb
@@ -0,0 +1,11 @@
+class Dream < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true, uniqueness: true, length: {minimum: 10, message: "please dream more than 10 caracters"}
+  validates :description, presence: true, uniqueness: true, length: {minimum: 10, message: "please dream more than 10 caracters"}
+  validates :price_per_night, presence: true
+  CATEGORIES = ["action", "success", "lust", "dreamy", "adventure", "travel", "fantasy", "time traveler" ]
+  validates :category, inclusion: {in: CATEGORIES}
+  validates :age_limit, presence: true
+  ROLES = (1..10)
+  validates :number_of_roles, inclusion: {in: ROLES}
+end
