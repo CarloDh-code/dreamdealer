e2593a6f72a8f8aa7da0bc6a5e71f3c5415e228a
Taib Falilou Sall <staibfalilou@gmail.com>
2024-11-21 17:45:16 +0100

    solved

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,14 +4,8 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :dream
 
-<<<<<<< HEAD
-  validates :start_date, :end_date, presence: true
-  def total_price
-    return 0 unless end_date.present? && start_date.present? && price_per_night.present?
-=======
   validates :start_date, presence: true
   validates :end_date, presence: true
->>>>>>> master
 
   def total_price
     return 0 unless end_date.present? && start_date.present? && self.dream.price_per_night.present?
